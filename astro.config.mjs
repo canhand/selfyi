67343dcb60c2a5b3a9e8ddc39392785d72b84fa1
chandan-gupta <chandangupta8@gmail.com>
2023-03-21 05:56:56 +0530

    staging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ufo.ckg.fyi/',
+  publicDir: './public',
+  outDir: './dist',
+  vite: {
+    plugins: [
+      {
+        name: 'import.meta.url-transformer',
+        transform: (code, id) => {
+          if (id.endsWith('.astro'))
+            return code.replace(/import.meta.url/g, `"${id}"`);
+        },
+      },
+    ],
+    ssr: {
+      external: ['svgo'],
+    },
+  },
+  experimental: { integrations: true },
+  integrations: [astroImageTools],
+  server: { port: 3001 },
+});
